9a1b73657979ef3ce2f801aa278c89dab3b56a00
Mohamed AbouDeif <M.AboDaif@outlook.com>
2025-09-13 03:58:43 +0300

    feat(test): add dcoker-compose.yaml for testing and e2e.sh

---
+++ source-code/frontend/10-nginx-conf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+envsubst '$DOMAIN_NAME $BACKEND_HOST $BACKEND_PORT ${REACT_APP_API_BASE_URL}' \
+        < /etc/nginx/templates/nginx.conf.tmp \
+        > /etc/nginx/nginx.conf
+
+echo "#########################################"
+echo "### nginx.conf generated successfully ###"
+echo "#########################################"
